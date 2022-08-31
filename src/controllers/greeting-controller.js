6a7f475194e14c9b3f863f45b294c2e15b627cb1
Fredrik Jurgell <fj222qp@student.lnu.se>
2022-08-31 12:29:58 +0200

    Added getGif-method and index.hbs.

--- src/controllers/greeting-controller.js
+++ src/controllers/greeting-controller.js
@@ -5,6 +5,8 @@
  * @version 1.0.0
  */
 
+import fetch from 'node-fetch'
+
 /**
  * Encapsulates a controller.
  */
@@ -19,6 +21,38 @@ export class GreetingController {
   async index (req, res, next) {
     try {
       res.render('greeting/index')
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  /**
+   * Displays the gif.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   */
+  async getGif (req, res, next) {
+    try {
+      const giphyApi = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=hello`
+    
+      const response = await fetch(giphyApi, {
+        method: 'GET'
+      })
+
+      const jsonData = await response.json()
+
+      // Get a random index.
+      const randomGif = Math.floor(Math.random() * jsonData.data.length)
+
+      // The viewdata.
+      const viewData = {
+        name: req.body.name,
+        gif: jsonData.data[randomGif].images.original.url
+      }
+
+      res.render('greeting/index', { viewData })
     } catch (error) {
       next(error)
     }
